1b9066364a266ed7aa487fec951b7ae351140aed
maK1naR1a <rbr@usal.es>
2025-02-27 17:26:17 +0100

    JUlian

--- Monticulos/monticulo.c
+++ Monticulos/monticulo.c
@@ -1,1 +1,15 @@
+#include "monticulo.h"
 
+void iniciaMonticulo(Monticulo *m)
+{
+}
+
+int vacioMonticulo(Monticulo m)
+{
+    return 0;
+}
+
+int insertar(tipoElemento x, Monticulo *m)
+{
+    return 0;
+}
